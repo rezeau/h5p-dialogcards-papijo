c20f554161edf7e8d4eb0d9b509395d3210ea954
rezeau <joseph@rezeau.org>
2023-12-27 16:01:48 +0100

    version 1.16.12 added .eslintrc.json modified entries/h5p-dialogcards.js
    Signed-off-by: rezeau <joseph@rezeau.org>

--- src/entries/h5p-dialogcards.js
+++ src/entries/h5p-dialogcards.js
@@ -1,6 +1,6 @@
 import "../styles/h5p-dialogcards.css";
-import "../scripts/h5p-dialogcards.js";
+import Dialogcards from "../scripts/h5p-dialogcards.js";
 
 // Load library
-// H5P = H5P || {};
-// H5P.Dialogcards = Dialogcards;
+H5P = H5P || {};
+H5P.Dialogcards = Dialogcards;
