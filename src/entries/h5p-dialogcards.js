1c34591e778f6a8596260da82585944db7d5a8c2
rezeau <joseph@rezeau.org>
2026-04-13 16:52:19 +0200

    wip05 replaced old H5P.DialogcardsPapiJo = (function ($, Audio, JoubelUI) {} with modern class DialogcardsPapiJo extends H5P.EventDispatcher {} - NEEDS REFINING

--- src/entries/h5p-dialogcards.js
+++ src/entries/h5p-dialogcards.js
@@ -1,2 +1,6 @@
 import '../styles/h5p-dialogcards.css';
 import DialogcardsPapiJo from '../scripts/h5p-dialogcards.js';
+
+// Load library
+// H5P = H5P || {};
+H5P.DialogcardsPapiJo = DialogcardsPapiJo;
